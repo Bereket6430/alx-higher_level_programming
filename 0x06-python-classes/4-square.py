dae4097924f83152c1e04b5a86f5d1f9a7e539a4
Bereket6430 <beksecured1@gmail.com>
2023-12-19 11:53:31 -0800

    square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -40,4 +40,4 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = 
+        self.__size =  
