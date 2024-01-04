46cef5b96233f0c1e6fb8322a5bd35f483ff3114
Bereket6430 <beksecured1@gmail.com>
2024-01-04 15:31:41 -0800

    NQueen

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -58,5 +58,5 @@ if __name__ == "__main__":
                 else:
                     nqueens(x + 1)  # moves on to next x value to continue
 
-    # start the recursive process at x = 0
+    #start the recursive process at x = 0
     nqueens(0)
